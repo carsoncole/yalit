b720c300286b774a07a145af810c7afa18af1fa9
Carson Cole <carson@grade.us>
2019-11-03 13:47:39 -0800

    added Procfile

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   test "a user can be created" do
     assert create(:user)
   end
+
+  test "user-created projects remain after destroying user" do
+    user = create(:user)
+    project = create(:project)
+    project.user_projects.build(user: user)
+
+    user.destroy
+    assert Project.find(project.id)
+    asset_not User.find_by(id: user.id)
+  end
 end
+
