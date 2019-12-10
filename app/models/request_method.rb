e906c3d91b9284a4169056f9b9f5a22b4eb7f5de
Carson Cole <carson@grade.us>
2019-12-10 10:56:42 -0800

    fix

--- app/models/request_method.rb
+++ app/models/request_method.rb
@@ -28,8 +28,8 @@ class RequestMethod < ApplicationRecord
       headers = {}
       headers = headers.merge({ "Authorization" => server.authorization_header }) if server.authorization_header.present?
       headers = headers.merge({ "Content-Type" => server.content_type_header}) if server.authorization_header.present?
-      response = HTTParty.get(request, headers: headers)
-      update(response_content: response.body, response_code: response.code)
+      response = HTTParty.get(request, headers: headers) rescue nil
+      update(response_content: response.nil? ? "Failed" : response.body, response_code: response.nil? ? 500 : response.code)
     end
   end
 end
