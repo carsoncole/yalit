cdcff4e3ff6bd130c5cd45433d9d74100ac26ae2
Carson Cole <carson@grade.us>
2020-01-16 12:43:35 -0800

    removed parameter tests as model has changed

--- test/models/parameter_test.rb
+++ test/models/parameter_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 #TODO Parameter tests
 class ParameterTest < ActiveSupport::TestCase
-  test "key must be present" do
-    parameter = build(:parameter, key: nil)
-    assert_not parameter.save, "Saved a parameter without a key"
-  end
+  # test "key must be present" do
+  #   parameter = build(:parameter, key: nil)
+  #   assert_not parameter.save, "Saved a parameter without a key"
+  # end
 
-  test "value must be present" do
-    parameter = build(:parameter, value: nil)
-    assert_not parameter.save, "Saved a parameter without a value"
-  end
+  # test "value must be present" do
+  #   parameter = build(:parameter, value: nil)
+  #   assert_not parameter.save, "Saved a parameter without a value"
+  # end
 
-  test "key must be unique for request methods" do
-    request_method = create(:request_method)
-    create(:parameter, key: "user_id", request_method_id: request_method.id)
-    parameter = build(:parameter, key: "user_id", request_method_id: request_method.id)
-    assert_not parameter.save, "Saved a non-unique parameter key within a request method"
-  end
+  # test "key must be unique for request methods" do
+  #   request_method = create(:request_method)
+  #   create(:parameter, key: "user_id", request_method_id: request_method.id)
+  #   parameter = build(:parameter, key: "user_id", request_method_id: request_method.id)
+  #   assert_not parameter.save, "Saved a non-unique parameter key within a request method"
+  # end
 end
