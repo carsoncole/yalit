78926ad762f7314ae78ef4e9a2c123f58a4ed29b
Carson Cole <carson.cole@gmail.com>
2022-10-17 13:38:57 -0700

    clearance fixes and added clearance routes

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ This an API document hosting and testing service.
 
 * Configuration
 
-Credentials are managed with Environment Variables including the following
+
+** Action Mailer
+Action mailer settings for user account administration is set in each respective environment configuration file.
+
+
+** Hosting
+Default configuration is to use Heroku for DNS resolution to white-labeled API documentation. The necessary environment variables that are required are:
 
 - HEROKU_APP_NAME
 - HEROKU_API_KEY
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get 'params/index'
-  get 'params/show'
-  get 'params/edit'
-  get 'params/update'
-  get 'params/create'
-  get 'params/destroy'
+
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
   get 'sign_up' => "users#new", as: 'noname'
   constraints Clearance::Constraints::SignedIn.new do
     root to: 'projects#index', as: :signed_in_root
